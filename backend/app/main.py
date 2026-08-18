4b9cf5ec8062fec7faea2383b12c026821640773
bestorbust <priyadarshinip658@gmail.com>
2026-08-18 12:14:48 +0530

    updated main

--- backend/app/main.py
+++ backend/app/main.py
@@ -33,7 +33,7 @@ app.add_middleware(
         settings.frontend_url,
         "http://localhost:5173",
         "http://127.0.0.1:5173",
-        "https://rentledger-neon.vercel.app/
+        "https://rentledger-neon.vercel.app/"
     ],
     allow_credentials=True,
     allow_methods=["*"],
